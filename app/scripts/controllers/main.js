e40bb55b1dda0524b5bd07bd633c3d5575c4a927
Jorge Luque <rev.volu.cion@gmail.com>
2015-10-25 16:10:30 +0100

    Mi primer commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mbpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mbpApp
+ */
+angular.module('mbpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
